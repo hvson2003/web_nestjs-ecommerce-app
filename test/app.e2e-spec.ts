834da60573d510babcd2cac16a954c8576ab866c
hvson03 <hvson2003@gmail.com>
2024-10-13 17:20:06 +0700

    Create tests for authentication routes with Jest framework and supertest

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,25 +1,77 @@
+import { HttpStatus } from '@nestjs/common';
 import 'dotenv/config';
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { RegisterDTO } from 'src/auth/auth.dto';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import mongoose from 'mongoose';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+const app = 'http://localhost:3000';
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+beforeAll(async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connection.db.dropDatabase();
+});
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+afterAll(async () => {
+  await mongoose.disconnect();
+});
+
+describe('ROOT', () => {
+  it('should ping', () => {
+    return request(app).get('/').expect(200).expect('Hello World!');
+  });
+});
+
+describe('AUTH', () => {
+  it('should register', () => {
+    const user: RegisterDTO = {
+      username: 'username1',
+      password: 'password',
+    };
+
+    return request(app)
+      .post('/auth/register')
+      .set('Accept', 'application/json')
+      .send(user)
+      .expect(({ body }) => {
+        expect(body.token).toBeDefined();
+        expect(body.user.username).toEqual('username1');
+        expect(body.user.password).toBeUndefined();
+      })
+      .expect(HttpStatus.CREATED);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('should reject duplicate registration', () => {
+    const user: RegisterDTO = {
+      username: 'username1',
+      password: 'password',
+    };
+
+    return request(app)
+      .post('/auth/register')
+      .set('Accept', 'application/json')
+      .send(user)
+      .expect(({ body }) => {
+        expect(body.message).toEqual('User already exists');
+        expect(body.code).toEqual(HttpStatus.BAD_REQUEST);
+      })
+      .expect(HttpStatus.BAD_REQUEST);
+  });
+
+  it('should login', () => {
+    const user: RegisterDTO = {
+      username: 'username1',
+      password: 'password',
+    };
+
+    return request(app)
+      .post('/auth/login')
+      .set('Accept', 'application/json')
+      .send(user)
+      .expect(({ body }) => {
+        expect(body.token).toBeDefined();
+        expect(body.user.username).toEqual('username1');
+        expect(body.user.password).toBeUndefined();
+      })
+      .expect(HttpStatus.CREATED);
   });
 });
