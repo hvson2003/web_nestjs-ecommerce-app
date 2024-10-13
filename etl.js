c1716c3c8ef15ad606ab9bf02d3019eef6e847f8
hvson03 <hvson2003@gmail.com>
2024-10-13 18:03:46 +0700

    Complete auth module for sellers and user authentication

--- etl.js
+++ etl.js
@@ -3,12 +3,18 @@ const axios = require('axios');
 
 (async () => {
   try {
-    const { data } = await axios.post('http://localhost:3000/auth/login', {
-      username: 'username',
+    const { data } = await axios.post('http://localhost:3000/auth/register', {
+      username: 'username1',
       password: 'password',
+      seller: true,
     });
-
     console.log(data);
+
+    const { token } = data;
+    const { data: res2 } = await axios.get('http://localhost:3000/auth', {
+      headers: { authorization: `Bearer ${token}` },
+    });
+    console.log(res2);
   } catch (error) {
     console.error('Error:', error.response?.data || error.message);
   }
